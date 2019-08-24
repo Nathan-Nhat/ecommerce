f8cdcb2076bb7fcaec9766ab72db036cf4265c42
Nathan-Nhat <trantrungnhat6196@gmail.com>
2019-08-24 12:34:11 -0700

    first commit

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import MenuItem from "../meni-item/menu-item.component"
+import './directory.style.scss'
+export default class Directory extends Component {
+    constructor(){
+        super();
+        this.state = {
+            section : [
+                {
+                    title : 'hats',
+                    imageUrl : 'https://i.ibb.co/cvpntL1/hats.png',
+                    id : 1,
+                },
+                {
+                    title : 'jeckets',
+                    imageUrl : 'https://i.ibb.co/px2tCc3/jackets.png',
+                    id : 2,
+                },
+                {
+                    title : 'snakers',
+                    imageUrl : 'https://i.ibb.co/0jqHpnp/hats.png',
+                    id : 3,
+                },
+                { 
+                    title : 'womens',
+                    imageUrl : 'https://i.ibb.co/GCCdy8t/womens.png',
+                    size : 'large',
+                    id : 4,
+                },
+                {
+                    title : 'mens',
+                    imageUrl : 'https://i.ibb.co/R70vBrQ/mens.png',
+                    size : 'large',
+                    id : 5,
+                }
+            ]
+        }
+    }
+    render() {
+        return (
+                 <div className = "directory-menu">
+                    {
+                        this.state.section.map(({title, imageUrl, id, size}) =>
+                            <MenuItem key = {id} title = {title} 
+                            imageUrl ={imageUrl} size = {size}/>
+                        )
+                    }
+            </div>
+        )
+    }
+}
